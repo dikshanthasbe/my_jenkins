92f3b1ba194ebbb1c07e54ac05c79cdda49c39a7
Dikshant Hasbe <117030317+dikshanthasbe@users.noreply.github.com>
2025-08-17 12:08:17 +0530

    Updated jenkins.ts

--- src/types/jenkins.ts
+++ src/types/jenkins.ts
@@ -87,15 +87,3 @@ export interface SyncResponse {
   sync_duration: number;
   last_sync_time: string;
 }
-
-export interface ApiResponse<T> {
-  data: T;
-  message?: string;
-  error?: string;
-}
-
-export interface SyncResponse {
-  jobs_synced: number;
-  sync_duration: number;
-  last_sync_time: string;
-}
